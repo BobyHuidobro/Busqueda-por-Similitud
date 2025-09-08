279c18c603e9255c892b7b35705692af31ca25e8
BobyHuidobro <dhuidobro@miuandes.cl>
2025-09-08 17:24:45 -0300

    bases

--- eda_cpp-main/clustering/cpp/include/simsearch.hpp
+++ eda_cpp-main/clustering/cpp/include/simsearch.hpp
@@ -1,15 +1,16 @@
 #ifndef _SIM_SEARCH_HPP
 #define _SIM_SEARCH_HPP
+#include "cluster.hpp"
 
 class SimSearch{
 private:
     const Matrix &mat_data; // data to be processed
-    const Matrix &mat_clusters; // centroids to be used
+    const Cluster &mat_clusters; // centroids to be used
 
 public:
-    SimSearch(Matrix, clusters);
-    std::vector<size_t> search_with_clusters(const float *query,  top_k);
-    std::vector<size_t> search_without(const float *query, top_k);
-}
+    SimSearch(const Matrix &_mat_data, const Cluster &_mat_clusters);
+    std::vector<size_t> search_with_clusters(const float *query, size_t top_k);
+    std::vector<size_t> search_without(const float *query, size_t top_k);
+};
 
 #endif 
--- eda_cpp-main/clustering/cpp/src/simsearch.cpp
+++ eda_cpp-main/clustering/cpp/src/simsearch.cpp
@@ -1,1 +1,21 @@
-//TODO
+#include "simsearch.hpp"
+
+SimSearch::SimSearch(const Matrix &_mat_data, const Cluster &_mat_clusters): mat_data(_mat_data), mat_clusters(_mat_clusters) {
+    // Constructor implementation (if needed)
+}
+
+std::vector<size_t> SimSearch::search_with_clusters(const float *query, size_t top_k) {
+    // Implementation of search using clusters
+    std::vector<size_t> results;
+    // ... (search logic here)
+
+    return results;
+}
+
+std::vector<size_t> SimSearch::search_without(const float *query, size_t top_k) {
+    // Implementation of search without using clusters
+    std::vector<size_t> results;
+    // ... (search logic here)
+
+    return results;
+}
